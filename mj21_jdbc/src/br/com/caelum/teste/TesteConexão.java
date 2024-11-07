f8d21b7129493d93c4780d1f1ab7d77192afae20
aluno <aluno@DESKTOP-AO9HOJV>
2024-11-07 19:40:19 -0300

    Teste

---
+++ mj21_jdbc/src/br/com/caelum/teste/TesteConexão.java
@@ -0,0 +1,12 @@
+package br.com.caelum.teste;
+
+import br.com.caelum.jdbc.ConnectionFactory;
+
+public class TesteConexão extends ConnectionFactory {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
